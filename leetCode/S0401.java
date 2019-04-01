179deb46828c2c32069e9ac0884aaa1697ed9628
onTed <1103062661@qq.com>
2019-04-01 11:33:03 +0800

    leetcode-28. Implement strStr()。

---
+++ leetCode/S0401.java
@@ -0,0 +1,22 @@
+package leetCode;
+
+public class S0401 {
+	// Input: haystack = "hello", needle = "ll"
+	// Output: 2 j<haystack.length() &&
+	public int strStr(String haystack, String needle) {
+		int l1 = haystack.length(), l2 = needle.length();
+		if (l1 < l2) {
+			return -1;
+		} else if (l2 == 0) {
+			return 0;
+		}
+		int threshold = l1 - l2;
+		for (int i = 0; i <= threshold; ++i) {
+			if (haystack.substring(i, i + l2).equals(needle)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+}
